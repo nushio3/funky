4357acf23c66ebd12cc7269a882ba92d21d7b919
Takayuki Muranushi <muranushi@gmail.com>
2013-05-02 00:10:56 +0900

    update funky.

--- Funky/Machine.hs
+++ Funky/Machine.hs
@@ -1,6 +1,27 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Funky.Machine where
 
-import Data.Vector
+import           Data.Default
+import qualified Data.Vector as V
+
+data Inst a
+  = Nop
+  | Unary (a -> a) Int
+  | Binary (a -> a -> a) Int Int
+
 
 data Machine a = Machine
-  { instructions :: Vector a }
+  { instructions :: V.Vector (Inst a) }
+
+eval :: forall a. Default a => Machine a -> V.Vector a
+eval Machine insts = ret
+  where
+    ret :: V.Vector a
+    ret = V.map compute insts
+
+    compute :: Inst a
+    compute inst = case inst of
+      Nop -> def
+      Unary f a0 -> f $ get a0
+      Binary f a0 a1 -> f (get a0) (get a1)
