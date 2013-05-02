1e7d97ec8d59a9384ae6e56b3367956b2faeb78a
Takayuki Muranushi <muranushi@gmail.com>
2013-05-02 15:02:23 +0900

    test script added

--- Funky/Machine.hs
+++ Funky/Machine.hs
@@ -3,10 +3,12 @@
 module Funky.Machine where
 
 import           Data.Default
+import           Data.Maybe
 import qualified Data.Vector as V
 
 data Inst a
   = Nop
+  | Imm a
   | Unary (a -> a) Int
   | Binary (a -> a -> a) Int Int
 
@@ -14,14 +16,24 @@ data Inst a
 data Machine a = Machine
   { instructions :: V.Vector (Inst a) }
 
+{-|
+
+'eval' turns the list of instructions into vector of results.
+
+-}
+
 eval :: forall a. Default a => Machine a -> V.Vector a
-eval Machine insts = ret
+eval (Machine insts) = ret
   where
     ret :: V.Vector a
-    ret = V.map compute insts
+    ret = V.imap compute insts
 
-    compute :: Inst a
-    compute inst = case inst of
+    compute :: Int -> Inst a -> a
+    compute idx inst = case inst of
       Nop -> def
-      Unary f a0 -> f $ get a0
-      Binary f a0 a1 -> f (get a0) (get a1)
+      Imm f -> f
+      Unary f a0 -> f $ get (idx-a0)
+      Binary f a0 a1 -> f (get $ idx-a0) (get $ idx-a1)
+
+    get :: Int -> a
+    get addr = maybe def id (ret V.!? addr)
