a6b7d153fab249beaa4c5e7d19c1f155de095ec2
Takayuki Muranushi <muranushi@gmail.com>
2013-05-03 18:03:59 +0900

    added tests

---
+++ test/doctests.hs
@@ -0,0 +1,29 @@
+module Main where
+
+import Control.Applicative
+import Control.Monad
+import Test.DocTest
+import System.Directory
+import System.FilePath
+
+findHs :: FilePath -> IO [FilePath]
+findHs dir = do
+  fs <- map (dir </>) <$>
+    filter (`notElem` ["..","."]) <$>
+    getDirectoryContents dir
+  subDirs <- filterM doesDirectoryExist fs
+  files1 <-
+    filter ((/='.') . head . takeFileName) <$>
+    filter ((`elem` [".hs", ".lhs"]) . takeExtension) <$>
+    filterM doesFileExist fs
+  files2 <- concat <$> mapM findHs subDirs
+  return $ files1 ++ files2
+
+main :: IO ()
+main = do
+  files <-
+    fmap concat $
+    mapM findHs ["Funky"]
+
+  putStrLn $ "testing: " ++ unwords files
+  doctest files
